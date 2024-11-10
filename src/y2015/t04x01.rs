4d978cfe2a32a0384806cfcc198edd6d0333cd08
James Gopsill <james.gopsill@gmail.com>
2024-11-10 21:17:21 +0000

    2015 Day 04

---
+++ src/y2015/t04x01.rs
@@ -0,0 +1,36 @@
+use md5::{Digest, Md5};
+
+pub fn invoke(
+	input: String,
+	_debug: bool,
+) -> u32 {
+	for i in 0..9_999_999 {
+		let mut hasher = Md5::new();
+		let secret = format!("{}{}", input.trim(), i);
+		hasher.update(secret.as_bytes());
+		let result = hasher.finalize();
+		let hash = format!("{:x}", result);
+		if hash.starts_with("00000") {
+			println!("{} {}", secret, hash);
+			return i;
+		}
+	}
+	0
+}
+
+#[cfg(test)]
+mod tests {
+	use super::invoke;
+
+	#[test]
+	fn test_a() {
+		let result = invoke("abcdef".to_string(), true);
+		assert_eq!(result, 609043);
+	}
+
+	#[test]
+	fn test_b() {
+		let result = invoke("pqrstuv".to_string(), true);
+		assert_eq!(result, 1048970);
+	}
+}
---
+++ src/y2015/t04x02.rs
@@ -0,0 +1,19 @@
+use md5::{Digest, Md5};
+
+pub fn invoke(
+	input: String,
+	_debug: bool,
+) -> u32 {
+	for i in 0..9_999_999 {
+		let mut hasher = Md5::new();
+		let secret = format!("{}{}", input.trim(), i);
+		hasher.update(secret.as_bytes());
+		let result = hasher.finalize();
+		let hash = format!("{:x}", result);
+		if hash.starts_with("000000") {
+			println!("{} {}", secret, hash);
+			return i;
+		}
+	}
+	0
+}
